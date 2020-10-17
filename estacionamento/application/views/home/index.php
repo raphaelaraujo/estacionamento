eb40a46ffde5dd08075c9a17f3faefa9175710dc
Raphael Araújo <raphaelaraujo075@gmail.com>
2020-10-16 21:12:04 -0300

    Seção2: Módulo - Preparando nosso ambiente - aula 7

---
+++ estacionamento/application/views/home/index.php
@@ -0,0 +1,26 @@
+
+            <?php $this->load->view('layout/navbar');?>
+
+            <div class="page-wrap">
+
+                <?php $this->load->view('layout/sidebar');?>
+
+                <div class="main-content">
+                    <div class="container-fluid">
+                        
+                    <h1>HOME</h1>
+
+                    </div>
+                </div>
+
+
+                <footer class="footer">
+                    <div class="w-100 clearfix">
+                        <span class="text-center text-sm-left d-md-inline-block">Copyright © 2018 ThemeKit v2.0. Todos os direitos reservados.</span>
+                        <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Criado com <i class="fa fa-heart text-danger"></i> por <a href="https://softbean.com.br" class="text-dark" target="_blank">Softbean</a></span>
+                    </div>
+                </footer>
+                
+            </div>
+        
+        
