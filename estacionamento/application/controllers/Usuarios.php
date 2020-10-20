d2961d12a88841ec1cf9ae45f35de4027108aaef
raphaelaraujo <raphaelaraujo075@gmail.com>
2020-10-20 17:38:55 -0200

    Mostrando o perfil de acesso ao usuário - Aula 13

---
+++ estacionamento/application/controllers/Usuarios.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Usuarios extends CI_Controller{
+
+	public function index(){
+
+		$data = array(
+			'titulo' => 'Usuários cadastrados',
+			'subtitulo' => 'Chegou a hora de listar todos os usuários cadastrados no banco de dados',
+
+			'styles' => array(
+				'plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+			),
+
+			'scripts' => array(
+				'plugins/datatables.net/js/jquery.dataTables.min.js',
+				'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+				'plugins/datatables.net/js/estacionamento.js',
+			),
+
+			'usuarios' =>  $this->ion_auth->users()->result(), // get all users
+		);
+
+		/*
+		echo '<pre>';
+		print_r($data['usuarios']);
+		exit();
+		*/
+
+		$this->load->view('layout/header', $data);
+		$this->load->view('usuarios/index');
+		$this->load->view('layout/footer');
+	}
+}
---
+++ estacionamento/application/views/usuarios/index.php
@@ -0,0 +1,93 @@
+<?php $this->load->view('layout/navbar');?>
+
+<div class="page-wrap">
+
+    <?php $this->load->view('layout/sidebar');?>
+
+    <div class="main-content">
+                    <div class="container-fluid">
+                        <div class="page-header">
+                            <div class="row align-items-end">
+                                <div class="col-lg-8">
+                                    <div class="page-header-title">
+                                        <i class="ik ik-users bg-blue"></i>
+                                        <div class="d-inline">
+                                            <h5><?php echo $titulo ?></h5>
+                                            <span><?php echo $subtitulo ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <nav class="breadcrumb-container" aria-label="breadcrumb">
+                                        <ol class="breadcrumb">
+                                            <li class="breadcrumb-item">
+                                                <a title="Home" href="<?php echo base_url('/') ?>"><i class="ik ik-home"></i></a>
+                                            </li>
+                                            <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo ?></li>
+                                        </ol>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-header"><a href="#" class="btn btn-success"> + Novo</a></div>
+                                    <div class="card-body">
+                                        <table class="table data_table">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Usuário</th>
+                                                    <th>E-mail</th>
+                                                    <th>Nome</th>
+                                                    <th>Perfil de acesso</th>
+                                                    <th>Ativo</th>
+                                                    <th class="nosort text-right pr-25">Ações</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach($usuarios as $user): ?>
+                                                <tr>
+                                                    <td><?php echo $user->id ?></td>
+                                                    <td><?php echo $user->username ?></td>
+                                                    <td><?php echo $user->email ?></td>
+                                                    <td><?php echo $user->first_name ?></td>
+                                                    <td><?php echo ($this->ion_auth->is_admin($user->id) ? 'Administrador' : 'Atendente') ?>
+                                                    </td>
+                                                    <td><?php echo $user->active == 1 ? 
+                                                    '<span class="badge badge-pill badge-success mb-1">Sim</span>' : 
+                                                    '<span class="badge badge-pill badge-danger mb-1">Não</span>'?></td>
+                                                    <td class="text-right">
+                                                        <a title="Editar usuário" href="#" class="btn btn-icon btn-primary">
+                                                            <i class="ik ik-edit"></i>
+                                                        </a>
+                                                        <a title="Excluir usuário" href="#" class="btn btn-icon btn-danger">
+                                                            <i class="ik ik-trash-2"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+
+
+    <footer class="footer">
+        <div class="w-100 clearfix">
+            <span class="text-center text-sm-left d-md-inline-block">Copyright © <?php echo date('Y')?> ThemeKit v2.0. Todos os direitos reservados.</span>
+            <span class="float-none float-sm-right mt-1 mt-sm-0 text-center">Criado com <i class="fa fa-heart text-danger"></i> por <a href="https://softbean.com.br" class="text-dark" target="_blank">Softbean</a></span>
+        </div>
+    </footer>
+    
+</div>
+        
+        
